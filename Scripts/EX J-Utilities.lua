7c98585d17db678f675c257418479086a843506a
Jose_Varela <jo.lu6853@gmail.com>
2018-03-27 15:03:48 -0600

    Added some icons, utilities and now struggling with Menu choice shapes

--- Scripts/EX J-Utilities.lua
+++ Scripts/EX J-Utilities.lua
@@ -10,6 +10,8 @@ function Center(self)
 	self:y(SCREEN_CENTER_Y)
 end
 
+function ThemeFile( file ) return THEME:GetPath( EC_GRAPHICS, '' , file ) end
+
 function ColCon(n1, n2, n3)
 	return (n1/255),(n2/255),(n3/255),1
 end
@@ -18,3 +20,17 @@ end
 --function WideScale(AR4_3, AR16_10) return scale( SCREEN_WIDTH, 640, 960, AR4_3, AR16_10 ); end
 function WideScale(AR4_3, AR16_10) return scale( SCREEN_WIDTH, 640, 768, AR4_3, AR16_10 ); end
 --function WideScale(AR4_3, AR16_9) return scale( SCREEN_WIDTH, 640, 854, AR4_3, AR16_9 ); end
+
+TitleMenu_OptionColors = {
+	HSVA(128, 0.6, 0.325, 0.8),	-- play
+	HSVA(192, 0.6, 0.325, 0.8),	-- options
+	HSVA( 64, 0.6, 0.325, 0.8),	-- edit
+	HSVA(  0, 0.6, 0.325, 0.8),	-- exit
+};
+
+TitleMenu_OptionColorsFocused = {
+	HSVA(128,0.6,0.85,1),	-- play
+	HSVA(192,0.6,0.85,1),	-- options
+	HSVA(64,0.6,0.85,1),	-- edit
+	HSVA(0,0.6,0.85,1),	-- exit
+};
