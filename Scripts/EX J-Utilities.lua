0ba7d22a89e549c626c76bf7951636276f2d66ec
Jose_Varela <jo.lu6853@gmail.com>
2018-03-27 22:56:06 -0600

    tons of new things and stuffffff

--- Scripts/EX J-Utilities.lua
+++ Scripts/EX J-Utilities.lua
@@ -47,6 +47,32 @@ function DiffuseHSV(name)
 	return name[index+1][1],name[index+1][2],name[index+1][3],name[index+1][4]
 end
 
+function ParticleCommandInit(self)
+		self:blend('add');
+		self:diffusealpha(0.25);
+		self:queuecommand('Reset');
+	end
+	function ParticleCommandOn(self)
+		self:spin();
+		self:effectperiod(spinPeriod);
+		self:effectmagnitude(0,0,90);
+		self:queuecommand('Float');
+	end
+	function ParticleReset(self)
+		local randX = math.random(SCREEN_WIDTH*0.05,SCREEN_WIDTH*0.95);
+		local randZoom = math.random(0.25,1);
+		self:x(randX);
+		self:y(SCREEN_BOTTOM + math.random(32,128));
+		self:zoom(randZoom);
+	end
+	function ParticleFloat(self)
+		self:linear(0.5*pWeight);
+		self:y(-(SCREEN_HEIGHT*0.75));
+		self:sleep(math.random(1,3));
+		self:playcommand('Reset');
+		self:queuecommand('Float');
+	end
+
 OptionColors = {
 	HSVA(128, 0.6, 0.275, 0.53125),	-- play
 	HSVA(192, 0.6, 0.275, 0.53125),	-- options
